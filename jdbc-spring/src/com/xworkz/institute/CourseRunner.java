8efa2288adf3793b42e8b91d6be2359a54355b8b
SnehaPawar027 <sneha.xworkz36@gmail.com>
2022-06-02 01:41:05 +0530

    1.06.2022

--- jdbc-spring/src/com/xworkz/institute/CourseRunner.java
+++ jdbc-spring/src/com/xworkz/institute/CourseRunner.java
@@ -15,15 +15,15 @@ public class CourseRunner {
 
 		CourseService courseService = new CourseServiceImpl(courseDAO);
 
-		CourseDTO dto1 = new CourseDTO(1, "Java", 6, 23500.00d, LocalDate.of(2022, 6, 16), true);
+		CourseDTO dto1 = new CourseDTO(1, "Java", 3, 23500.00d, LocalDate.of(2022, 6, 16), true);
 		CourseDTO dto2 = new CourseDTO(2, "BootStrack", 2, 1500.00d, LocalDate.of(2022, 6, 16), true);
 		CourseDTO dto3 = new CourseDTO(3, "Selenium", 2, 7000.00d, LocalDate.of(2022, 6, 16), true);
 		CourseDTO dto4 = new CourseDTO(4, "Dot-Net", 4, 1000.00d, LocalDate.of(2022, 6, 16), true);
 
 		courseService.validate(dto1);
-		// courseService.validate(dto2);
-		// courseService.validate(dto3);
-		// courseService.validate(dto4)
+		 courseService.validate(dto2);
+		 courseService.validate(dto3);
+		 courseService.validate(dto4);
 
 	}
 
